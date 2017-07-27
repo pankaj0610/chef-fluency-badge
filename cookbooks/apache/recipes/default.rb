4b8a6de49a8150e6b9e4656b395976426c413f43
Pankaj <pankaj_poddar85@yahoo.co.in>
2017-07-27 16:14:26 +0000

    added motd and changed apaache default for the node attributes after correcting the error in default.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,9 +5,9 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 if node['platform_family'] == "rhel"
-	package 'httpd'
+	package = 'httpd'
 elsif node['platform_family'] == "debian"
-	package 'apache2'
+	package = 'apache2'
 end
 
 package 'apache' do
